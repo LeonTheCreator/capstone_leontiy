c6e1d9ed7513df2a347efcd006b353f0cd21b75c
LeonTheCreator <84448821+LeonTheCreator@users.noreply.github.com>
2021-09-23 14:10:22 +0200

    Backend für User

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
-import HomePage from '.pages/HomePage'
-import Registration from '.pages/Registration'
-import Watchlist from '.pages/Watchlist'
+import Registration from './pages/Registration'
+import AuthProvider from './auth/AuthProvider'
+import ProtectedRoute from './auth/ProtectedRoute'
+
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/registration">
-          <Registration />
-        </Route>
-        <Route path="/watchlist">
-          <Watchlist />
-        </Route>
-        <Route path="/news"></Route>
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route path="/registration" component={Registration} />
+          <ProtectedRoute exact path="/" component={HomePage} />
+          <ProtectedRoute path="/change-password" component={ChangePassword} />
+        </Switch>
+      </Router>
+    </AuthProvider>
   )
 }
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,1 +1,4 @@
 // hier soll einfach nur der Login-Button und die Registrierung sein + PW zurücksetzen
+export default function HomePage() {
+  return <h1>Hallo</h1>
+}
