4d8936387ab1ad86b3ea6e5699de9216dc2811fd
LeonTheCreator <84448821+LeonTheCreator@users.noreply.github.com>
2021-09-26 22:14:37 +0200

    WatchlistEntity mit den Post und Delete

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,6 +3,7 @@ import TextField from '../components/TextField'
 import Button from '../components/Button'
 import Header from '../components/Header'
 import Page from '../components/Page'
+import { useState } from 'react'
 /*
 const initialState = {
   username: '',
@@ -15,11 +16,24 @@ const [error, setError] = useState()
 */
 
 export default function Login() {
+  const [username, setUsername] = useState('')
+  const handleUsernameChange = event => {
+    const newUsername = event.target.value
+    setUsername(newUsername)
+  }
+  const handleSubmit = event => {
+    event.preventDefault()
+  }
   return (
     <Page>
       <Header title="Login" />
-      <Main as="form">
-        <TextField title="Username" name="username" />
+      <Main as="form" onSubmit={handleSubmit}>
+        <TextField
+          title="Username"
+          name="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
         <TextField title="Password" name="password" type="password" />
         <Button>login</Button>
       </Main>
