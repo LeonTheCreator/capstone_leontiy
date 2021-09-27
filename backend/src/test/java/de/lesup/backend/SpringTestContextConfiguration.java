d93ecb0e5fc2913ffcb9a58844c2eef5a9dda498
LeonTheCreator <84448821+LeonTheCreator@users.noreply.github.com>
2021-09-27 10:54:37 +0200

    Datenbankltrick für Githiub

---
+++ backend/src/test/java/de/lesup/backend/SpringTestContextConfiguration.java
@@ -0,0 +1,22 @@
+package de.lesup.backend;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@EnableAutoConfiguration
+@TestConfiguration
+public class SpringTestContextConfiguration {
+
+    @Primary
+    @Bean(name = "dataSource", destroyMethod = "shutdown")
+    public EmbeddedDatabase dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+}
